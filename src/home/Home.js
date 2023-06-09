01160a71ea0be1f3b84d4d5d488eb21a59acca40
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-09 14:23:54 +0530

    api data fetch and display on ui screen

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,23 @@
 import "./Home.scss";
-import Hoc from "../hoc/Hoc";
-
+import React, { useState, useEffect } from "react";
 function Home(probs) {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => {
+        setList(data);
+      });
+  }, []);
+  console.log(list);
   return (
-    <div>
-      <button onClick={probs.incerement}>clickhere${probs.count}</button>
+    <div className="app">
+      {list.map((item) => {
+        debugger;
+        return <p>{item.title}</p>;
+      })}
     </div>
   );
 }
 // export default Home;
-export default Hoc(Home);
+export default Home;
