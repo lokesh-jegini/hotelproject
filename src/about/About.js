6443dcce67a74105b0095cfdca1c07abecc50f66
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-09 10:01:49 +0530

    path params

--- src/about/About.js
+++ src/about/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './About.scss'
+  import { useParams } from "react-router-dom";
+  
 export default function About() {
+   const data = useParams();
   return (
     <div>
       <h1>about</h1>
+     <h3>{data.name}</h3>
     </div>
   )
 }
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
+  const user="lokesh"
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/dashboard">Dashboard</Link>
-      <Link to="/about">About</Link>
+      <Link to={ `/about/${user}`}>About</Link>
     </div>
   );
 }
