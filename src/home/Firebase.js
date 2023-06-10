2b6c12ccf2f575685940a4b4f1793a895d24d6e4
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-10 10:54:33 +0530

    post opertation of firebase

--- src/home/Firebase.js
+++ src/home/Firebase.js
@@ -1,7 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-const firebaseConfig = {
+
+import firebase from "firebase";
+ const firebaseConfig = {
   apiKey: "AIzaSyBM32twPGnzzsv-Uu07LZJXj6fyt9iRY0M",
   authDomain: "register-6e570.firebaseapp.com",
   databaseURL: "https://register-6e570-default-rtdb.firebaseio.com",
@@ -9,10 +8,9 @@ const firebaseConfig = {
   storageBucket: "register-6e570.appspot.com",
   messagingSenderId: "325968421948",
   appId: "1:325968421948:web:81d500309758875bda0f39",
+
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const firebaseDB = firebase.firestore(); // Use firestore() instead of database()
-
-export default firebaseDB;
+const firebaseDB = firebase.initializeApp(firebaseConfig);
+export default firebaseDB.database().ref();
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.scss";
 import React, { useState } from "react";
-// import firebaseDB from "./Firebase.js";
+import firebaseDB from "./Firebase.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 function Home(probs) {
@@ -13,28 +13,29 @@ function Home(probs) {
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     debugger;
     e.preventDefault();
-    axios
-      .post(
-        "https://register-6e570-default-rtdb.firebaseio.com/register.json",
-        data
-      )
-      .then((res) => {
-        alert("data added successfully");
-      });
-    // firebaseDB
-    //   .collection("users")
-    //   .add(data)
-    //   .then(() => {
-    //     debugger
-    //     console.log(data);
-    //     console.log("Data added successfully!");
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error adding data: ", error);
+    // axios
+    //   .post(
+    //     "https://register-6e570-default-rtdb.firebaseio.com/register.json",
+    //     data
+    //   )
+    //   .then((res) => {
+    //     alert("data added successfully");
     //   });
+    var dataset=await firebaseDB.child("jegini").push(data, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        alert("data added successfully");
+      }
+    });
+    setData({
+       firstname: "",
+    lastname: "",
+    email: "",
+    });
   };
   return (
     <form onSubmit={handleSubmit}>
